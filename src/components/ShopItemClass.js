27558ccdd258125ece0a75e8001fae28296dba3d
Michael Veselov <michael.veselov@gmail.com>
2022-06-18 19:54:23 +0300

    init commit - task complete

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ShopItemClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}; // добавлено намеренно, чтобы VSCode не выдавал ошибку, что конструктор не нужен
+  }
+
+  render() {
+    const { brand, title, description, descriptionFull, price, currency } =
+      this.props.item;
+
+    return (
+      <div className='main-content'>
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div className='descriptionFull'>{descriptionFull}</div>
+        <div className='highlight-window-mobile'>
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className='divider'></div>
+        <div className='purchase-info'>
+          <div className='price'>{`${currency}${price}.00`}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ShopItemClass.propTypes = {
+  item: PropTypes.exact({
+    brand: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    descriptionFull: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+  }),
+};
+
+export default ShopItemClass;
